9678882fbe4020d3204c4e7853a43085dddce2e2
Anmol Verma <anmol.verma4@gmail.com>
2022-07-08 23:25:30 +0530

    fix: font family

--- app/src/main/java/dev/baseio/discordjetpackcompose/ui/theme/Type.kt
+++ app/src/main/java/dev/baseio/discordjetpackcompose/ui/theme/Type.kt
@@ -28,21 +28,5 @@ val UniSansFontFamily = FontFamily(
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = UniSansFontFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    defaultFontFamily = UniSansFontFamily
 )
