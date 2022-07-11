b20791ef50b3bb8f19a27262c17b60b75c95b166
Ruthvik B R <ruthvikbk@Ruthviks-MacBook-Pro.local>
2022-07-11 11:55:56 +0530

    Add invite screen

--- navigator/src/main/java/dev/baseio/discordjetpackcompose/navigator/Screens.kt
+++ navigator/src/main/java/dev/baseio/discordjetpackcompose/navigator/Screens.kt
@@ -14,6 +14,7 @@ sealed class DiscordScreen(
     object Register : DiscordScreen("register")
     object Login : DiscordScreen("login")
     object Dashboard : DiscordScreen("dashboard")
+    object Invite : DiscordScreen("invite")
 }
 
 sealed class DiscordRoute(val name: String) {
