a84c905ace1e9121c2ff09b39b6617a4bd0a45b9
Pushpal Roy <pushpalroy2007@gmail.com>
2022-07-19 13:57:35 +0530

    Add reply marker line

--- app/src/main/java/dev/baseio/discordjetpackcompose/ui/routes/dashboard/main/chatscreen/ChatMessageItem.kt
+++ app/src/main/java/dev/baseio/discordjetpackcompose/ui/routes/dashboard/main/chatscreen/ChatMessageItem.kt
@@ -1,6 +1,7 @@
 package dev.baseio.discordjetpackcompose.ui.routes.dashboard.main.chatscreen
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
@@ -20,6 +21,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.StrokeJoin.Companion
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.PlaceholderVerticalAlign
@@ -89,7 +95,9 @@ fun MessageItemReplyHeader(
   ListItem(
     modifier = Modifier
       .padding(0.dp),
-    icon = {},
+    icon = {
+      ReplyLine()
+    },
     text = {
       ChatReplyTitle(
         toUserName = "Person",
@@ -97,6 +105,42 @@ fun MessageItemReplyHeader(
       )
     }
   )
+}
+
+@Composable
+fun ReplyLine() {
+  Canvas(
+    modifier = Modifier
+      .size(50.dp)
+      .padding(0.dp)
+  ) {
+    val width = size.width
+    val height = size.height
+    val path = Path().apply {
+      moveTo(
+        x = width.times(1.2f),
+        y = height.times(.45f)
+      )
+      cubicTo(
+        x1 = width.times(0f),
+        y1 = height.times(.4f),
+
+        x2 = width.times(.35f),
+        y2 = height.times(1.5f),
+
+        x3 = width.times(.30f),
+        y3 = height.times(1.4f)
+      )
+    }
+    drawPath(
+      path = path,
+      color = Color.White.copy(alpha = .90f),
+      style = Stroke(
+        width = 2f,
+        join = StrokeJoin.Miter
+      )
+    )
+  }
 }
 
 @Composable
@@ -157,7 +201,7 @@ fun ChatReplyTitle(
     appendInlineContent(id = "imageId")
     pushStyle(
       SpanStyle(
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Medium,
         color = DiscordColorProvider.colors.textPrimary
       )
     )
